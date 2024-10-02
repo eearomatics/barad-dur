616df374113540955efb66b5226127395e68ac8d
Tom Carrio <tom@carrio.dev>
2024-10-02 19:04:29 -0400

    feat: bot user agent in lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       settings: {
-        extraHeaders: JSON.stringify({Referer: 'https://lhci.eearomatics.com'}),
+        extraHeaders: JSON.stringify({["User-Agent"]: 'Mozilla/5.0 (compatible; EE-Aromatics-BotScraper/1.0; +https://eearomatics.com/bot)'}),
       },
     },
   },
