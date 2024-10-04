2a77b60588bcab73d13e5b7bf424f7f7b8b74db2
Tom Carrio <tom@carrio.dev>
2024-10-03 21:35:26 -0400

    feat: use chromeFlags in lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       settings: {
-        extraHeaders: JSON.stringify({["User-Agent"]: 'Mozilla/5.0 (compatible; EE-Aromatics-BotScraper/1.0; +https://eearomatics.com/bot)'}),
+        chromeFlags: '--user-agent="Mozilla/5.0 (compatible; EE-Aromatics-BotScraper/1.0; +https://eearomatics.com/bot)"',
       },
     },
   },
